cb667ad882ed5803a4a0762020f1126452bb90dd
RPengCheng <135105534+RPengCheng@users.noreply.github.com>
2024-02-02 22:42:01 +0800

    Delete dao directory

--- dao/models.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from tortoise import Model,fields
-class Todo(Model):
-    """数据库中的表todo"""
-    id = fields.IntField(pk=True)
-    contest = fields.CharField(max_length=500)
-    created_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(auto_now=True)
